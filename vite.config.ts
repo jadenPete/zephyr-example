df19dd51ea4986db3e67c806e79a0a622e4e17e5
jadenPete <jadenpete@github.com>
2026-02-25 00:51:02 -0500

    Added a project

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { withZephyr } from "vite-plugin-zephyr";
+
+export default defineConfig({
+  plugins: [react(), withZephyr()],
+});
